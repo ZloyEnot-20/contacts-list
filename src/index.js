7124eb72bb50b584834837ed7f5b0fd8416ea5a1
oleg_kadambaev <oleg.kadambaev@gmail.com>
2021-09-02 20:52:06 +0500

    almost production version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { allReducer } from './reducers';
+
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import usersReducer from './reducers/usersReducer';
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import usersReducer from '../reducers/usersReducer'
-
-
-export const allReducer = combineReducers({
-    users : usersReducer
-})
-
